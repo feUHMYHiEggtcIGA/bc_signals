924cf55c6819d8005934c44668f0057ec3c4340c
Your Name <you@example.com>
2025-10-03 18:07:40 +0200

    test updt

--- tests/unit/train/mm.rs
+++ tests/unit/train/mm.rs
@@ -1,20 +1,21 @@
-use bc_utils_lg::statics::prices::SRC;
+use bc_utils_lg::statics::prices::{SRC, OPEN};
 
 use bc_signals::train::mm::*;
 
 #[test]
 fn mm_coll_res_1(){
-    // test it thoroughly
-    assert_eq!(
-        vec![-1.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-        mm_coll::<Vec<f64>>(
-            &SRC[..10], 
-            "open",
-            &2, 
-            &3,
-            &0.0001,
-            &0.01,
-        ),
+    let res =  mm_coll::<Vec<f64>>(
+        SRC.as_slice(), 
+        "open",
+        &2, 
+        &3,            
+        &0.0001,
+        &0.01,
     );
-
+    assert_eq!(
+        &[-1.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0],
+        &res[..10]
+    );
+    assert_eq!(&-1.0, &res[OPEN.iter().enumerate().max_by(|v1, v2| v1.1.partial_cmp(v2.1).unwrap()).unwrap().0],);
+    assert_eq!(&1.0, &res[OPEN.iter().enumerate().min_by(|v1, v2| v1.1.partial_cmp(v2.1).unwrap()).unwrap().0],);
 }
